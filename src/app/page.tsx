17561cd9ae213b8629a07f99d009c73d1cd29a7b
Miloš Ćemalović <99266701+Cemalovic@users.noreply.github.com>
2024-10-23 12:47:29 +0200

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,25 @@
+import { Divider } from '@nextui-org/react'
+import TopicCreateForm from '@/components/topics/TopicCreateForm'
+import TopicList from '@/components/topics/TopicList'
+import PostList from '@/components/posts/PostList'
+import { fetchTopPosts } from '@/db/queries/posts'
+
+const Home = () => {
+  return (
+    <div className='grid grid-cols-4 gap-4 p-4'>
+      <div className='col-span-3'>
+        <h1 className='text-xl m-2'>Top Posts</h1>
+        <PostList fetchData={fetchTopPosts} />
+      </div>
+
+      <div className='border shadow py-3 px-2'>
+        <TopicCreateForm />
+        <Divider className='my-2' />
+        <h3 className='text-lg'>Topics</h3>
+        <TopicList />
+      </div>
+    </div>
+  )
+}
+
+export default Home
